21840e8340167e406038e73cbd31dc6d3e1b4bf7
Iino Ten <nteten0313@icloud.com>
2019-05-22 14:18:47 +0900

    Add get now place

--- src/container/NowPlace.js
+++ src/container/NowPlace.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 import Map from '../components/map'
+import { GoogleMap } from 'react-google-maps';
+import axios from 'axios'
 
 class NowPlace extends Component{
   constructor(props){
@@ -17,6 +19,21 @@ class NowPlace extends Component{
           lat: position.coords.latitude, 
           lng: position.coords.longitude 
         })
+        //https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCQKM0wFvX4rvb_E0qErN5i6DZa7yMT6TM&latlng=55.755826,37.6173&sensor=false
+        this.props.change_coordinate(position.coords.latitude, position.coords.longitude);
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json?key=' + process.env.REACT_APP_GOOGLE_MAPS_API + '&latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=false')
+          .then((res)=>{
+            let address_name = res.data.results
+            let full_address
+            for (let i = 0; i < address_name.length; i++) {
+              full_address += address_name[i].formatted_address;
+              console.log(address_name[i].formatted_address)
+            }
+            console.log( "住所は",address_name[9].formatted_address)
+          })
+          .catch((err) => {
+            console.log("Error:",err)
+          })
       },
       (err) => {
         console.log("Failed get positon:", err)
