f1860d428e46f72c6999d282012c208cfb587def
Iino Ten <nteten0313@icloud.com>
2019-05-22 14:18:12 +0900

    Add Start Button

---
+++ src/components/StartButton/StartButton.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import './StartButton.css'
+
+/*マテリアルUI導入*/
+import Button from '@material-ui/core/Button'
+
+/**マテリアルアイコン導入 */
+
+class StartButton extends Component{
+  render(){
+    return( 
+      <Button
+          variant="contained"
+          size="Large"
+          color="primary"
+          aria-label="Add"
+          className="start-button"
+          id="start-button"
+        >
+          はじめる
+        </Button>
+
+    );
+  }
+}
+
+export default StartButton;
