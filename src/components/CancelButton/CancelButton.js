d5ee05b46bfece1a0bf1fd638d82c7f8f7d746df
Iino Ten <nteten0313@icloud.com>
2019-05-27 17:19:13 +0900

    Update cancel button

--- src/components/CancelButton/CancelButton.js
+++ src/components/CancelButton/CancelButton.js
@@ -6,19 +6,45 @@ import Button from '@material-ui/core/Button'
 
   
   class CancelButton extends Component{
+    constructor(props) {
+      super();
+    }
+    onClick_stop_button_handler = () => {
+      this.props.onClick_stop_button()
+    }
+    onClick_cancel_button_handler = () => {
+      console.log("cancel button")
+      this.props.onCick_cancel_button()
+    }
     render(){
-      return(
+      let cancel_Button = 
         <Button 
           variant="outlined" 
           size="medium" 
           color="primary" 
           className="cancel-button" 
           id="cancel-button"
-        >
-          おわる
-        </Button>
+          onClick={this.onClick_cancel_button_handler}>
+            キャンセルする({this.props.cancelable_count }) 
+          </Button>;
+      let finish_Button =
+        <Button 
+          variant="outlined" 
+          size="medium" 
+          color="primary" 
+          className="cancel-button" 
+          id="cancel-button"
+          onClick={this.onClick_stop_button_handler}>
+          おわる 
+        </Button>;
+      return(
+        <div>
+        {this.props.cancelable_count ? cancel_Button : finish_Button}
+        </div>
       );
     }
   }
+
+  
   
   export default CancelButton;
