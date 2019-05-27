381bd1b8f741ca97db894ecada827b48d07f0cfe
Iino Ten <nteten0313@icloud.com>
2019-05-27 21:12:08 +0900

    Add popup report

---
+++ src/components/PopupReport/PopupReport.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import './PopupReport.css'
+
+/*マテリアルUI導入*/
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+
+class PopupReport extends Component{
+  render(){
+    return(
+      <div>
+        <Dialog open={this.props.open}aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+        </DialogActions>
+      </Dialog>
+      </div>
+    );
+  }
+}
+
+export default PopupReport;
