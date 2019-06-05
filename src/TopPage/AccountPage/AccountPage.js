b4ebcc1841f260e8a1bb68c580c5aa3b2888e0ca
Iino Ten <nteten0313@icloud.com>
2019-06-05 22:11:04 +0900

    Add account page menu button

--- src/TopPage/AccountPage/AccountPage.js
+++ src/TopPage/AccountPage/AccountPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import firebase from 'firebase'
 
 import SignInScreen from '../../components/SignInScreen/SignInScreen'
+import ReadyLogin from '../../components/ReadyLogin/ReadyLogin'
 
 import './AccountPage.css'
 
@@ -10,12 +11,20 @@ class AccountPage extends Component{
     super();
     this.state={
       loading: true,
-      user: null
+      user: null,
+
+      user_photoURL: '',
+      user_name: '',
+      user_profile: ''
     }
   }
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
-      console.log("hello",user)
+      console.log("hello",user.photoURL.replace('normal.jpg', '400x400.jpg'))
+      let user_photoURL = user.photoURL.replace('normal.jpg', '400x400.jpg')
+      console.log(user)
+      let user_name = user.displayName;
+      this.setState({ user_photoURL, user_name })
       if(user){
         firebase.firestore().collection('users').doc(user.uid).get()
           .then((doc)=>{
@@ -64,10 +73,12 @@ class AccountPage extends Component{
       if (this.state.loading)return <div>loading</div>;
     return (
       <div>
-        Username: {this.state.user && this.state.user.displayName}
         <br />
         {this.state.user ?
-          (<button onClick={this.logout}>Logout</button>) :
+          (<ReadyLogin 
+            user_name={this.state.user_name}
+            user_photoURL={this.state.user_photoURL}
+          />) :
           (<SignInScreen />)
         }
       </div>
---
+++ src/components/ReadyLogin/ReadyLogin.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+import ShowChart from '@material-ui/icons/ShowChart'
+import SettingsTwoTone from '@material-ui/icons/SettingsTwoTone'
+import LiveHelpOutlined from '@material-ui/icons/LiveHelpOutlined'
+
+import './ReadyLogin.css'
+
+
+
+class ReadyLogin extends Component{
+  render(){
+    console.log(this.props.user_name)
+    return(
+      <div>
+        <Grid container justify="center" alignItems="center">
+          <Avatar 
+            alt="Remy Sharp" 
+            src={this.props.user_photoURL} 
+            style={{margin: 10, width: 120, height: 120, marginTop: 50}}
+          />
+        </Grid>
+        <Grid container justify="center" alignItems="center">
+          <Typography variant="h6">{this.props.user_name}</Typography>
+        </Grid>
+        <Grid container justify="center" alignItems="center">
+          <List>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ShowChart />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="もくもくレポート" secondary="最近のもくもく，今までのもくもくした履歴" />
+              </ListItem>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar>
+                    <SettingsTwoTone />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="設定" secondary="アカウントプロフィールの変更や各種の設定" />
+              </ListItem>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar>
+                    <LiveHelpOutlined />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="このサービスについて" secondary="もくもくappについて" />
+              </ListItem>
+          </List>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default ReadyLogin;
